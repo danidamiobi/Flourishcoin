8b370b420d820091985fc1f845d8b8c22603bfba
Daniel Damilola Obiokeke <danencyclopedia@gmail.com>
2021-11-16 15:25:33 +0100

    Update Flourish Coin.sol

--- Flourish Coin.sol
+++ Flourish Coin.sol
@@ -416,7 +416,7 @@ interface IUniswapV2Router02 is IUniswapV2Router01 {
     ) external;
 }
 
-contract FlourishCoin is Context, IERC20, Ownable {
+contract BrokenThumb is Context, IERC20, Ownable {
     using SafeMath for uint256;
     using Address for address;
     
